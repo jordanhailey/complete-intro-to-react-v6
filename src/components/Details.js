e594a28be10b0da5481553b823c7afaed5a66c3f
jordanhailey <jordan.m.hailey@gmail.com>
2021-03-11 15:46:49 -0800

    Implementing a Modal, available on any page

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,8 +3,9 @@ import { withRouter } from 'react-router-dom';
 import ThemeContext from './ThemeContext';
 import Carousel from './Carousel';
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from './Modal';
 class Details extends Component {
-  state = {loading:true}
+  state = {loading:true, showModal: false}
 
   async componentDidMount () {
     const res = await fetch(
@@ -16,12 +17,17 @@ class Details extends Component {
     }, json.pets[0]))
   }
 
+  toggleModal = () => {
+    this.setState({showModal: !this.state.showModal});
+  }
+  adopt = () => (window.location = "http://bit.ly/pet-adopt")
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>
     }
 
-    const {animal, breed, name, city, state, description, images } = this.state;
+    const {animal, breed, name, city, state, description, images, showModal } = this.state;
     return (
       <div className="details">
         <Carousel images={images}/>
@@ -29,10 +35,25 @@ class Details extends Component {
         <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
         <ThemeContext.Consumer>
           {([theme,]) => (
-            <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>
+            Adopt {name}
+          </button>
           )}
         </ThemeContext.Consumer>
         <p>{description}</p>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null
+        }
       </div>
     );
   }
