8a84d9f76333438527fbaf45a526861225eba685
jordanhailey <jordan.m.hailey@gmail.com>
2021-03-11 14:02:37 -0800

    creating a class component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,26 @@
-const Details = () => {
-  return <h2>Hello Details</h2>
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Details extends Component {
+  constructor () {
+    super();
+
+    this.state = { loading : true };
+  }
+
+  async componentDidMount () {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({
+      loading: false
+    }, json.pets[0]))
+  }
+
+  render() {
+    return (<pre>{JSON.stringify(this.state,null,2)}</pre>);
+  }
 }
 
-export default Details;
+export default withRouter(Details);
