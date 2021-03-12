39237c5cd038c47746065da19bfb1cfeb773aa60
jordanhailey <jordan.m.hailey@gmail.com>
2021-03-12 12:52:58 -0800

    adding Suspense and lazy from react to lazy load component js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+const SearchParams = lazy(() => import("./SearchParams"))
+const Details = lazy(() => import("./Details"))
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
@@ -11,21 +11,23 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>Loading Route ...</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
